57897f78c02536d4b7e195cc4e58cd8b64ad77ca
Denike-analyst <163006247+Denike-analyst@users.noreply.github.com>
2024-05-23 12:57:52 +0100

    Update Adult_mortality_rate.sql

--- Adult_mortality_rate.sql
+++ Adult_mortality_rate.sql
@@ -9,7 +9,7 @@ SELECT * FROM health_expenditure
 SELECT * FROM GDP
 
 
---African countries where population in 2021 was greater than 75000 and their health expenditure for 2019 was lower than 35
+-- The result from this query shows that 32 African countries had a population greater than 75000 in 2021 and their health expenditure for 2019 was lower than 35.
 SELECT p.Countries, a.Continent FROM Population p
 INNER JOIN health_expenditure h ON p.Countries = h.Countries
 INNER JOIN Adult_mortality_rate a ON a.Countries = h.Countries
@@ -17,13 +17,13 @@ WHERE Continent = 'Africa'
 AND p.Year_2021 > 75000 AND h.Year_2019 < 35
 
 
---SHOW THE TOP 5 EUROPEAN COUNTRIES IN TERMS OF GDP AND LOW FEMALE ADULT MORTALITY RATE
+--The result from this query shows the top 5 European countries in terms of GDP and low female adult mortality rate
 SELECT TOP 5 g.Countries, a.Continent FROM GDP g
 INNER JOIN Adult_mortality_rate a ON a.Countries = g.Countries
 WHERE Continent = 'Europe'
 ORDER BY GDP DESC, AMR_female ASC
 
---Among the Asian countries in the top 10 for population in 2019, theses are the 3 countries with lowest health expenditures. 
+--The result from this query shows the Asian countries in the top 10 for population in 2019, where 3 of these countries have the lowest health expenditures. 
 
 SELECT TOP 3 h.Year_2019 AS hexp_2019, h.Countries FROM health_expenditure h
 INNER JOIN (
@@ -36,14 +36,15 @@ INNER JOIN (
 WHERE h.Year_2019 IS NOT NULL
 ORDER BY h.Year_2019 ASC
 
---
 
---Getting the total average of both male and female adult mortality rate for continents
+--The result from this query shows the total average of both male and female adult mortality rate for continents
+      
 SELECT Continent, CEILING(SUM(AMR_female)) AS total_female, CEILING(SUM(AMR_male)) AS total_male 
 FROM Adult_mortality_rate
 GROUP BY Continent
 
---Showing the relation between health expenditure and crude death rate between 2019, 2020, and 2021
+--The result from this query shows the relation between health expenditure and crude death rate between 2019, 2020, and 2021 for all the countries\
+      
 SELECT h.Countries, CEILING(h.Year_2019) AS hexp_19, CEILING(h.Year_2020) AS hexp_20, CEILING(h.Year_2021) AS hexp_21, 
 CEILING(c.Year_2019) AS dr_19, CEILING(c.Year_2020) AS dr_19, CEILING(c.Year_2021) AS dr_2021
 FROM health_expenditure h INNER JOIN crude_death_rate c
